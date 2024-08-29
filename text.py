e3db7340e56a4c29c6212b4d4939bbe865698e2d
aruniankur <arunitowardnew@gmail.com>
2024-08-29 16:31:52 +0530

    change in the bracnh

--- text.py
+++ text.py
@@ -1,1 +1,14 @@
 print("hell world")
+for i in range(0,100):
+    print("Welcom to the git")
+    
+a = 3
+b = 2
+    
+while True:
+    if a < b:
+        break
+    else:
+        a = a - b
+        print(a)
+        print("hell world")
