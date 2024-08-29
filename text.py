bb42b050af8ae3f084ae7b2269c05640e8c5c5c3
aruniankur <arunitowardnew@gmail.com>
2024-08-29 16:35:44 +0530

    changes in the branch

--- text.py
+++ text.py
@@ -2,13 +2,13 @@ print("hell world")
 for i in range(0,100):
     print("Welcom to the git")
     
-a = 3
-b = 2
+a = 300
+b = 21
     
 while True:
     if a < b:
         break
     else:
-        a = a - b
+        a = a - 1
         print(a)
         print("hell world")
