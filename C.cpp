7da0b0650130ab90dfc5e5f5d3014a9b3db8be2f
aruniankur <arunitowardnew@gmail.com>
2024-08-29 16:57:52 +0530

    fixing the std error

--- C.cpp
+++ C.cpp
@@ -1,7 +1,15 @@
-#include <stdc++.h>
+#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main(){
+    vector<int> val;
+    val.push_back(1);
+    val.push_back(2);
+    val.push_back(3);
+    for(int i=0; i<val.size();i++){
+        cout << val[i] << endl;
+    }
     cout << "Running" << endl;
     return 0;
 }
